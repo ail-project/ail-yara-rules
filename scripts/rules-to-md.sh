fa2f718fe542018c65c06428d9aacfa97e2a8f9e
Alexandre Dulaunoy <a@foo.be>
2020-07-29 10:50:00 +0200

    chg: [doc] fix path generator

--- scripts/rules-to-md.sh
+++ scripts/rules-to-md.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ../rules
+cd ..
 
 tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
-       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
+       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' | grep -v README | grep -v scripts | grep -v LICENSE)
 
 printf "# YARA rules\n\n${tree}"

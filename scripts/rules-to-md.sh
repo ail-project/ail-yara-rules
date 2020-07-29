899bfd61d1055c34da2ca9401d00a628efc19559
Alexandre Dulaunoy <a@foo.be>
2020-07-29 10:46:03 +0200

    chg: [doc] a simple generator to list of all YARA rules

---
+++ scripts/rules-to-md.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ../rules
+
+tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
+
+printf "# YARA rules\n\n${tree}"
